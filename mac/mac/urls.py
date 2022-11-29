433703fe65f386e8748d5d64f2a9a59fbb1f1436
vivek <mrl9viveksahu@gmail.com>
2022-11-29 15:19:57 +0530

    Search Page

--- mac/mac/urls.py
+++ mac/mac/urls.py
@@ -22,7 +22,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
-    path('blog/', include('blog.urls')),
     path('',views.index)
     
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
